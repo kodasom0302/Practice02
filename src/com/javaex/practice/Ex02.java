74d66915c2d645bd4b6abd4b716c8b34fc05b3e4
dasom <dasom589@gmail.com>
2023-12-27 17:04:51 +0900

    2023-12-27

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,21 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+}
+/*
+다음 중 switch문에서 조건식에 넣을 수 있는 데이터 타입은?
+	(1) boolean
+	(2) char
+	(3) byte
+	(4) short
+	(5) int
+	(6) long
+	(7) float
+	(8) double
+	(9) String
+
+
+
+(2) (3) (4) (5) (9)
+*/
