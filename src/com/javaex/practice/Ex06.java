acccf479c96eec44b9e619193d3dab27e49d629d
dasom <dasom589@gmail.com>
2023-12-28 16:11:50 +0900

    2023-12-28

---
+++ src/com/javaex/practice/Ex06.java
@@ -0,0 +1,27 @@
+package com.javaex.practice;
+
+public class Ex06 {
+
+/*	
+나이를 입력받아 19세 이상 65세 미만이면 "1번그룹" 그 이외에는 "2번그룹"으로 출력하는 프로그램을 작성하세요.
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int age;
+		
+		System.out.println("나이를 입력해주세요");
+		System.out.print("나이: ");
+		age=sc.nextInt();
+		
+		if (18<age && age<65) {
+			System.out.println("1번그룹");
+		} else {
+			System.out.println("2번그룹");
+		}
+		
+		sc.close();
+
+	}
+*/
+}
