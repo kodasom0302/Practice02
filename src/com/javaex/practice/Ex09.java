f1e7c7fe9b22488d7861e3d7d8e1707d3db358f4
dasom <dasom589@gmail.com>
2023-12-28 16:12:35 +0900

    2023-12-28

---
+++ src/com/javaex/practice/Ex09.java
@@ -0,0 +1,32 @@
+package com.javaex.practice;
+
+public class Ex09 {
+
+/*	
+아래의 조건과 같이 출력되도록 프로그램을 작성하세요.
+	태어난 년도를 입력 받습니다.
+	나이는 생일과 관계없이 계산됩니다.	올해 2023년, 2023년생 -> 3살 (2023-2020=3)
+	15세 미만의 경우와 65세 이상인 경우에는 "(나이)살 무료예방접종 대상자 입니다" 메시지를 출력합니다.
+	이외의 경우에는 "(나이)살 무료예방접종 대상자가 아닙니다" 메시지를 출력합니다.
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("태어난 년도를 입력해주세요");
+		System.out.print("년도: ");
+		int year=sc.nextInt();
+		
+		int age=2023-year;
+		
+		if (age<15 || 64<age) {
+			System.out.println(age+"살 무료예방접종 대상자 입니다");
+		} else {
+			System.out.println(age+"살 무료예방접종 대상자가 아닙니다");
+		}
+		
+		sc.close();
+
+	}
+*/
+}
