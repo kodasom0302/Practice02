d42730b56b5ba1a07e8a0b10648e52a1324a34b6
dasom <dasom589@gmail.com>
2023-12-28 16:12:07 +0900

    2023-12-28

---
+++ src/com/javaex/practice/Ex07.java
@@ -0,0 +1,39 @@
+package com.javaex.practice;
+
+public class Ex07 {
+
+/*	
+나이를 입력받아 아래와 같이 입장료가 출력되도로 프로그램을 작성하세요.
+	조건			출력내용
+	0~7세		취학전아동/무료 입니다.
+	8~13세		초등학생/2000원 입니다.
+	14~16세		중학생/3000원 입니다.
+	17~19세		고등학생/4000원 입니다.
+	20세~		성인/5000원 입니다.
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int age;
+		
+		System.out.println("나이를 입력해주세요.");
+		System.out.print("나이: ");
+		age=sc.nextInt();
+		
+		if (age<8) {
+			System.out.println("취학전아동/무료 입니다.");
+		} else if (age<14) {
+			System.out.println("초등학생/2000원 입니다.");
+		} else if (age<17) {
+			System.out.println("중학생/3000원 입니다.");
+		} else if (age<20) {
+			System.out.println("고등학생/4000원 입니다.");
+		} else {
+			System.out.println("성인/5000원 입니다.");
+		}
+		
+		sc.close();
+
+	}
+*/
+}
---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,36 @@
+package com.javaex.practice;
+
+public class Ex08 {
+
+/*	
+사용자로 키와 몸무게를 입력받아 표준체중을 계산한 후에 저체중, 표준, 과체중을 구분하여 출력하는 프로그램을 작성하세요.
+표준체중 = (키-100)*0.9
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		double height, weight;
+		
+		System.out.println("키와 몸무게를 입력해주세요.");
+		System.out.print("키: ");
+		height=sc.nextDouble();
+		System.out.print("몸무게: ");
+		weight=sc.nextDouble();
+		
+		double result=(height-100)*0.9;
+		
+		if (result<weight) {
+			System.out.println("과체중 입니다.");
+		} else if (result>weight) {
+			System.out.println("저체중 입니다.");
+		} else {
+			System.out.println("표준 입니다.");
+		}
+		
+			System.out.println("표준체중: "+result);
+		
+		sc.close();
+
+	}
+*/
+}
