82cf9a4877c1869af0c795f3c9582b68b3381d51
dasom <dasom589@gmail.com>
2023-12-29 12:54:50 +0900

    2023-12-29

--- src/com/javaex/practice/확인용.java
+++ src/com/javaex/practice/확인용.java
@@ -6,11 +6,11 @@ public class 확인용 {
 
 	public static void main(String[] args) {
 		
-		String[] dArray=new String[3];
+		char [] dArray=new char[3];
 		
-		dArray[0]="A";
-		dArray[1]="B";
-		dArray[2]="C";
+		dArray[0]='a';
+		dArray[1]='b';
+		dArray[2]='c';
 		
 		for (int i=0; i<4; i++) {	//배열명[index] 축약 -> i
 			System.out.println(dArray[i]);
