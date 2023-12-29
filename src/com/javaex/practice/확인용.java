d53bc89c981a408c1b1953a56befd9a8d1144489
dasom <dasom589@gmail.com>
2023-12-29 12:50:45 +0900

    2023-12-29

--- src/com/javaex/practice/확인용.java
+++ src/com/javaex/practice/확인용.java
@@ -7,17 +7,20 @@ public class 확인용 {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
-		System.out.print("첫번째 숫자: ");
+		System.out.println("숫자 3개를 입력해주세요");
+		System.out.print("숫자1: ");
 		int no01=sc.nextInt();
-		System.out.print("두번째 숫자: ");
+		System.out.print("숫자2: ");
 		int no02=sc.nextInt();
+		System.out.print("숫자3: ");
+		int no03=sc.nextInt();
 		
-		if (no01>no02) {
-			System.out.println("몫: "+no01/no02);
-			System.out.println("나머지: "+no01%no02);
+		if (no01<no02 && no01<no03) {
+			System.out.println("가장 작은 수는 "+no01+" 입니다.");
+		} else if (no02<no01 && no02<no03) {
+			System.out.println("가장 작은 수는 "+no02+" 입니다.");
 		} else {
-			System.out.println("몫: "+no02/no01);
-			System.out.println("나머지: "+no02%no01);
+			System.out.println("가장 작은 수는 "+no03+" 입니다.");
 		}
 		
 		sc.close();
