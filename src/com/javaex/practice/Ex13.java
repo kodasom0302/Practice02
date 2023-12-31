85e9fed5f14d3a518800b6a46a64c6870822769d
dasom <dasom589@gmail.com>
2023-12-31 19:08:48 +0900

    2023-12-31

---
+++ src/com/javaex/practice/Ex13.java
@@ -0,0 +1,33 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex13 {
+
+/*	
+두 개의 정수를 입력받은 후에 작은 수가 큰 수의 약수인지 확인하는 프로그램을 작성하세요.
+(0은 입력하지 않습니다.)
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("첫번째 숫자: ");
+		int no01=sc.nextInt();
+		System.out.print("두번째 숫자: ");
+		int no02=sc.nextInt();
+		
+		if (no01>no02 && no01%no02==0) {
+			System.out.println(no02+"는(은) "+no01+"의 약수입니다.");
+		} else if (no01>no02 && !(no01%no02==0)) {
+			System.out.println(no02+"는(은) "+no01+"의 약수가 아닙니다.");
+		} else if (no01<no02 && no02%no01==0) {
+			System.out.println(no01+"는(은) "+no02+"의 약수입니다.");
+		} else {
+			System.out.println(no01+"는(은) "+no02+"의 약수가 아닙니다.");
+		}
+		
+		sc.close();
+
+	}
+*/
+}
