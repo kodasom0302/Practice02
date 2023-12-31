481560140887997f27462b9761784bae2ee8f27c
dasom <dasom589@gmail.com>
2023-12-31 19:09:31 +0900

    2023-12-31

--- src/com/javaex/practice/확인용.java
+++ src/com/javaex/practice/확인용.java
@@ -6,15 +6,20 @@ public class 확인용 {
 
 	public static void main(String[] args) {
 		
-		char [] dArray=new char[3];
+		Scanner sc=new Scanner(System.in);
+		System.out.println("숫자를 입력해 주세요");
+		System.out.print("숫자: ");
+		double x=sc.nextDouble();
+		double result01=x*x*x-9*x+2;
+		double result02=7*x+2;
 		
-		dArray[0]='a';
-		dArray[1]='b';
-		dArray[2]='c';
-		
-		for (int i=0; i<4; i++) {	//배열명[index] 축약 -> i
-			System.out.println(dArray[i]);
+		if (x<=0) {
+			System.out.println("계산 결과는 "+result01+" 입니다.");
+		} else {
+			System.out.println("계산 결과는 "+result02+" 입니다.");
 		}
+		
+		sc.close();
 		
 		}
 
