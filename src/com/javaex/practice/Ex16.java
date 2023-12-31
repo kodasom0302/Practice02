efe9839a834407f5054443c93fe3f07790c04734
dasom <dasom589@gmail.com>
2023-12-31 19:09:23 +0900

    2023-12-31

---
+++ src/com/javaex/practice/Ex16.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex16 {
+
+/*	
+다음과 같이 정의되는 함수의 함수값을 계산해보자
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("숫자를 입력해 주세요");
+		System.out.print("숫자: ");
+		double x=sc.nextDouble();
+		double result01=x*x*x-9*x+2;
+		double result02=7*x+2;
+		
+		if (x<=0) {
+			System.out.println("계산 결과는 "+result01+" 입니다.");
+		} else {
+			System.out.println("계산 결과는 "+result02+" 입니다.");
+		}
+		
+		sc.close();
+
+	}
+*/
+}
