d52405e3cff1e408f41b4b3fbf5b36cf423e33c2
dasom <dasom589@gmail.com>
2024-01-02 11:55:18 +0900

    2024-01-02

--- src/com/javaex/practice/확인용.java
+++ src/com/javaex/practice/확인용.java
@@ -6,40 +6,20 @@ public class 확인용 {
 
 	public static void main(String[] args) {
 		
-		double tax;
-		
 		Scanner sc=new Scanner(System.in);
-		System.out.println("수익을 입력해 주세요");
-		System.out.print("금익: ");
-		double revenue=sc.nextDouble();
+		System.out.println("태어난 년도를 입력해 주세요");
+		System.out.print("년도: ");
+		int year=sc.nextInt();
 		
-		switch (revenue) {
-			case (0==revenue || 0<revenue || revenue==1000 || revenue<1000) :
-				tax=0.09*revenue;
-				System.out.println("소득세는 "+tax+" 입니다.");
-				break;
-				
-			case (1000<revenue || revenue==4000 || revenue<4000) :
-				tax=1000*0.09 + 0.18*(revenue-1000);
-				System.out.println("소득세는 "+tax+" 입니다.");
-				break;
-				
-			case (4000<revenue || revenue<8000) :
-				tax=1000*0.09 + 3000*0.18 + 0.27*(revenue-4000);
-				System.out.println("소득세는 "+tax+" 입니다.");
-				break;
-				
-			case (8000==revenue || 8000<revenue) :
-				tax=1000*0.09 + 3000*0.18 + 4000*0.27 + 0.36*(revenue-8000);
-				System.out.println("소득세는 "+tax+" 입니다.");
-				break;
-				
-			default :
-				System.out.println("잘못 입력했습니다.");
-				break;
+		int age=2023-year;
+		
+		if (age<15 || age>=65) {
+			System.out.println(age+"살 무료예방접종 대상자 입니다");
+		} else {
+			System.out.println(age+"살 무료예방접종 대상자가 아닙니다");
 		}
-			
-			sc.close();
+		
+		sc.close();
 		
 		}
 
