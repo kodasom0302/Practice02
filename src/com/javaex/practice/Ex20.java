19002d93860d87fb056cc69e11ec3b11b5d717c8
dasom <dasom589@gmail.com>
2024-01-02 11:55:10 +0900

    2024-01-02

---
+++ src/com/javaex/practice/Ex20.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+
+public class Ex20 {
+
+/*	
+알파벳을 입력받아 자음 모음을 구분하세요.
+ if~else문을 사용합니다.
+ 영문 소문자 이외의 입력값은 고려하지 않습니다.
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println();
+		
+
+	}
+*/
+}
