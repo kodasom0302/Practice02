efc41e7aa7ef871385feb5cf6151c60b086cd4f0
dasom <dasom589@gmail.com>
2024-01-02 11:55:03 +0900

    2024-01-02

---
+++ src/com/javaex/practice/Ex18.java
@@ -0,0 +1,44 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex18 {
+
+/*	
+알파벳을 입력받아 자음 모음을 구분하세요.
+ switch~case문을 사용합니다.
+ 영문 소문자 이외의 입력값은 고려하지 않습니다.
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("알파벳 1글자(소문자)를 입력하세요");
+		System.out.print("알파벳: ");
+		String alphabet=sc.nextLine();
+		
+		switch (alphabet) {
+		case "a":
+			System.out.println("모음입니다.");
+			break;
+		case "e":
+			System.out.println("모음입니다.");
+			break;
+		case "i":
+			System.out.println("모음입니다.");
+			break;
+		case "o":
+			System.out.println("모음입니다.");
+			break;
+		case "u":
+			System.out.println("모음입니다.");
+			break;
+		default :
+			System.out.println("자음입니다.");
+		}
+		
+		sc.close();
+
+	}
+*/
+}
