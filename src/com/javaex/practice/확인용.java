bbd2adf915049649dc1735be414d8222599f0a60
dasom <dasom589@gmail.com>
2024-01-03 09:47:28 +0900

    2024-01-03

--- src/com/javaex/practice/확인용.java
+++ src/com/javaex/practice/확인용.java
@@ -10,13 +10,28 @@ public class 확인용 {
 		System.out.println("태어난 년도를 입력해 주세요");
 		System.out.print("년도: ");
 		int year=sc.nextInt();
-		
 		int age=2023-year;
-		
-		if (age<15 || age>=65) {
-			System.out.println(age+"살 무료예방접종 대상자 입니다");
+		System.out.println("올해 : 2023");
+		System.out.println("태어난 해 : "+year);
+		System.out.println("나이 : "+age);
+		System.out.println("====================================");
+		if (age>=20) {
+			System.out.println("20살 이상");
+			
+		if (year%2!=0) {
+				System.out.println("건강검진해");
 		} else {
-			System.out.println(age+"살 무료예방접종 대상자가 아닙니다");
+			System.out.println("건강검진해 아님");
+		} 
+		
+		if (age>=40) {
+			System.out.println("암 검사");
+		} else {
+			System.out.println("암 검사X");
+		}
+		
+		} else {
+			System.out.println("20살 미만 건강검진 대상이 아님");
 		}
 		
 		sc.close();
@@ -26,4 +41,9 @@ public class 확인용 {
 	}
 
 
-//System.out.println();
+/*
+Scanner sc=new Scanner(System.in);
+		System.out.println();
+		System.out.print();
+		int year=sc.nextInt();
+*/
