b91339e63c435be1234f35af7773dca004fbcd3c
dasom <dasom589@gmail.com>
2024-01-03 09:46:54 +0900

    2024-01-03

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -1,17 +1,36 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex20 {
 
-/*	
+/*	??? 더 간단하게 하는 방법은 없나?
 알파벳을 입력받아 자음 모음을 구분하세요.
  if~else문을 사용합니다.
  영문 소문자 이외의 입력값은 고려하지 않습니다.
 
 	public static void main(String[] args) {
 		
-		System.out.println();
-		System.out.println();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("알파벳 1글자(소문자)를 입력하세요");
+		System.out.print("알파벳: ");
+		String alphabet=sc.nextLine();
 		
+		if (alphabet.equals("a")) {
+			System.out.println("모음입니다.");
+		} else if (alphabet.equals("e")) {
+			System.out.println("모음입니다.");
+		} else if (alphabet.equals("i")) {
+			System.out.println("모음입니다.");
+		} else if (alphabet.equals("o")) {
+			System.out.println("모음입니다.");
+		} else if (alphabet.equals("u")) {
+			System.out.println("모음입니다.");
+		} else {
+			System.out.println("자음입니다.");
+		}
+		
+		sc.close();
 
 	}
 */
